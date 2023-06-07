f12c16e50e44bc472a2e75baf9187ddd67d07861
Denishkunjadiya <86820507+Denishkunjadiya@users.noreply.github.com>
2023-06-07 16:31:24 +0530

    add complate

--- src/pages/treeView.js
+++ src/pages/treeView.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import TreeView from '@mui/lab/TreeView';
 import * as muIcon from '@mui/icons-material';
@@ -9,7 +9,12 @@ import TreeItem from '@mui/lab/TreeItem';
 import { styled } from "@mui/material/styles";
 import Data from './data';
 import EditModel from './EditModel';
+import { useFormik } from 'formik';
 
+const schema = yup.object({
+    name: yup.string().min(2).required('name is required'),
+    parent: yup.string().required('select box is empty')
+})
 
 
 const TreeViewReact = () => {
@@ -19,19 +24,27 @@ const TreeViewReact = () => {
     const [data, setData] = useState(Data)
     const [open, setOpen] = useState(false);
 
-    const [name, setName] = useState()
-    const [parent, setParent] = useState()
     const [chake, setChecked] = useState()
     const [id, setId] = useState()
 
     const [editName, setEditName] = useState('');
 
-    const [errors, setErrors] = useState()
+    const initialValues = {
+        name: "",
+        parent: ""
+    }
 
-    const valid = yup.object().shape({
-        name: yup.string().required('Name is Required')
+    // -------------------- valid
+
+    const { errors, touched, values, handleBlur, handleChange, handleSubmit } = useFormik({
+        initialValues: initialValues,
+        validationSchema: schema,
+        onSubmit: (values, { resetForm }) => {
+            save()
+            resetForm();
+
+        }
     })
-
 
     // --------------- edit Data
 
@@ -116,29 +129,26 @@ const TreeViewReact = () => {
     // ------------------ Add Data
 
     const save = () => {
-        if (parent) {
+        if (values.parent) {
 
             let genId = Math.random().toString(10).slice(2)
-            const info = { 'id': genId, "name": name, child: [] }
-            valid.validate({ name })
-                .then(() => {
-                    const add = (aa) => {
-                        if (aa.name === parent) {
-                            aa.child.push(info);
-                            setName('')
-                            console.log("Add")
-                        } else {
-                            aa.child.map((item) => add(item))
-                        }
-                    }
-                    add(data)
-                }).catch((err) => {
-                    const validError = {};
-                    err.inner.forEach((error) => {
-                        validError[error.path] = error.message;
-                    })
-                    setErrors(validError)
-                })
+            const info = { 'id': genId, "name": values.name, child: [] }
+
+            const add = (aa) => {
+                if (aa.name === values.parent) {
+                    aa.child.push(info);
+                    values.name = ''
+                    values.parent = ''
+                    // touched.name = {}
+                    // touched.parent = {}
+                    console.log("Add")
+
+                    // return values
+                } else {
+                    aa.child.map((item) => add(item))
+                }
+            }
+            add(data)
 
         } else {
             console.log('Parent Not Found')
@@ -166,6 +176,10 @@ const TreeViewReact = () => {
         setExpanded(!expanded);
     };
 
+    useEffect(() => {
+        renderTree(data)
+    }, [handleChange])
+
     return (
 
         <>
@@ -192,30 +206,38 @@ const TreeViewReact = () => {
             <mui.Collapse in={expanded} timeout="auto" unmountOnExit>
                 <mui.CardContent>
 
-                    <mui.Typography sx={{ margin: 'auto 30px' }}>
-                        <mui.TextField required name='name' defaultValue="" value={name} onChange={(e) => setName(e.target.value)} id="standard-basic" label="Name" variant="standard" />
-                    </mui.Typography>
+                    <form onSubmit={handleSubmit}>
+                        <mui.Typography sx={{ margin: 'auto 30px' }}>
+                            {/* <mui.TextField required name='name' defaultValue="" value={name} onChange={(e) => setName(e.target.value)} id="standard-basic" label="Name" variant="standard" /> */}
+                            <mui.TextField name='name' defaultValue="" value={values.name} onChange={handleChange} onBlur={handleBlur} id="standard-basic" label="Name" variant="standard" />
+                            {/* {console.log(erro)} */}
+                            {touched.name && errors.name && <p style={{ color: 'red' }}>**{errors.name}</p>}
+                        </mui.Typography>
 
-                    <mui.Typography sx={{ margin: '10px 20px' }}>
-                        <mui.FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+                        <mui.Typography sx={{ margin: '10px 20px' }}>
+                            <mui.FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
 
-                            <mui.InputLabel id="demo-simple-select-standard-label">parent</mui.InputLabel>
-                            <mui.Select
-                                labelId="demo-simple-select-standard-label"
-                                id="demo-simple-select-standard"
-                                value={parent}
-                                onChange={(e) => setParent(e.target.value)}
-                                label="parent"
-                            >
-                                {optionTree(data)}
-                            </mui.Select>
+                                <mui.InputLabel id="demo-simple-select-standard-label">parent</mui.InputLabel>
+                                <mui.Select
+                                    labelId="demo-simple-select-standard-label"
+                                    id="demo-simple-select-standard"
+                                    value={values.parent}
+                                    onChange={handleChange}
+                                    name='parent'
+                                    label="parent"
+                                >
+                                    {optionTree(data)}
+                                </mui.Select>
+                                {touched.parent && errors.parent && <p style={{ color: 'red' }}>**{errors.parent}</p>}
+                            </mui.FormControl>
 
-                        </mui.FormControl>
-                    </mui.Typography>
+                        </mui.Typography>
 
-                    <mui.Typography sx={{ margin: '10px 20px' }}>
-                        <mui.Button variant="contained" onClick={save} >Add Data</mui.Button>
-                    </mui.Typography>
+                        <mui.Typography sx={{ margin: '10px 20px' }}>
+                            <mui.Button variant="contained" type='submit' >Add Data</mui.Button>
+                            {/* <mui.Button variant="contained" onClick={save} >Add Data</mui.Button> */}
+                        </mui.Typography>
+                    </form>
 
                 </mui.CardContent>
             </mui.Collapse>
